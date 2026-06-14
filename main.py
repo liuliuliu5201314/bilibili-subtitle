c105991ed4102a9e885a7f938461029147e9cefc
w我就是我你是谁 <15801082933@163.com>
2026-06-15 04:29:38 +0800

    Fix PORT environment variable

--- main.py
+++ main.py
@@ -150,4 +150,6 @@ def api_video():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    import os
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
